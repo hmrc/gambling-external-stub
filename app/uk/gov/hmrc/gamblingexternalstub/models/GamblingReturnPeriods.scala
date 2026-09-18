7f04b5ecb9dcb5d26496d001a209ee3602991cad
Javed <131012491+javed-iqbal1@users.noreply.github.com>
2026-09-18 10:48:49 +0100

    DTR-8285  Create Gambling stubs endpoint for: Get Return Periods

---
+++ app/uk/gov/hmrc/gamblingexternalstub/models/GamblingReturnPeriods.scala
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.gamblingexternalstub.models
+
+import play.api.libs.json._
+
+case class GamblingReturnPeriods(
+                                  mgdRegNumber: String,
+                                  returnPeriodsId: String,
+                                  nstpEndDate1: String,
+                                  nstpEndDate2: String,
+                                  nstpEndDate3: String,
+                                  nstpEndDate4: String,
+                                  nstpEndDate5: String,
+                                  nstpEndDate6: String,
+                                  nstpEndDate7: String,
+                                  nstpEndDate8: String,
+                                  isInLastNstp: String,
+                                  finalPeriodWarning: String,
+                                  hasExistingNstpValues: String,
+                                  systemDate: String
+                                )
+
+object GamblingReturnPeriodsFormats {
+
+  implicit val gamblingReturnPeriodsFormat: OFormat[GamblingReturnPeriods] =
+    Json.format[GamblingReturnPeriods]
+
+  def fullModel(
+                 mgdRegNumber: String
+               ): GamblingReturnPeriods =
+    GamblingReturnPeriods(
+      mgdRegNumber          = mgdRegNumber,
+      returnPeriodsId       = "1",
+      nstpEndDate1          = "14-OCT-24",
+      nstpEndDate2          = "14-JAN-25",
+      nstpEndDate3          = "15-APR-25",
+      nstpEndDate4          = "15-JUL-25",
+      nstpEndDate5          = "14-OCT-25",
+      nstpEndDate6          = "14-JAN-26",
+      nstpEndDate7          = "15-APR-26",
+      nstpEndDate8          = "17-JUL-26",
+      isInLastNstp          = "1",
+      finalPeriodWarning    = "0",
+      hasExistingNstpValues = "1",
+      systemDate            = "2026-05-31"
+    )
+
+  def partialModel(
+                    mgdRegNumber: String
+                  ): GamblingReturnPeriods =
+    GamblingReturnPeriods(
+      mgdRegNumber          = mgdRegNumber,
+      returnPeriodsId       = "2",
+      nstpEndDate1          = "31-MAR-26",
+      nstpEndDate2          = "",
+      nstpEndDate3          = "",
+      nstpEndDate4          = "",
+      nstpEndDate5          = "",
+      nstpEndDate6          = "",
+      nstpEndDate7          = "",
+      nstpEndDate8          = "",
+      isInLastNstp          = "0",
+      finalPeriodWarning    = "0",
+      hasExistingNstpValues = "1",
+      systemDate            = "2026-05-31"
+    )
+
+  def noDataModel(
+                   mgdRegNumber: String
+                 ): GamblingReturnPeriods =
+    GamblingReturnPeriods(
+      mgdRegNumber          = "",
+      returnPeriodsId       = "",
+      nstpEndDate1          = "",
+      nstpEndDate2          = "",
+      nstpEndDate3          = "",
+      nstpEndDate4          = "",
+      nstpEndDate5          = "",
+      nstpEndDate6          = "",
+      nstpEndDate7          = "",
+      nstpEndDate8          = "",
+      isInLastNstp          = "",
+      finalPeriodWarning    = "",
+      hasExistingNstpValues = "",
+      systemDate            = ""
+    )
+
+  def `XWM00000001770`: GamblingReturnPeriods =
+    fullModel("XWM00000001770")
+}
---
+++ test/uk/gov/hmrc/gamblingexternalstub/controllers/rdsDataCacheProxy/GamblingReturnPeriodsControllerSpec.scala
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.gamblingexternalstub.controllers.rdsDataCacheProxy
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.libs.json.Json
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import uk.gov.hmrc.gamblingexternalstub.base.SpecBase
+
+class GamblingReturnPeriodsControllerSpec
+  extends AnyWordSpec
+    with Matchers
+    with SpecBase {
+
+  private val app =
+    applicationBuilder().build()
+
+  private val controller =
+    app.injector.instanceOf[GamblingReturnPeriodsController]
+
+  "GamblingReturnPeriodsController#getReturnPeriods" should {
+
+    "return full return periods for XWM00000001770" in {
+
+      val result =
+        controller.getReturnPeriods(
+          "MGD",
+          "XWM00000001770"
+        )(FakeRequest())
+
+      status(result) shouldBe OK
+
+      val json =
+        contentAsJson(result)
+
+      (json \ "mgdRegNumber")
+        .as[String] shouldBe "XWM00000001770"
+
+      (json \ "returnPeriodsId")
+        .as[String] shouldBe "1"
+
+      (json \ "nstpEndDate1")
+        .as[String] shouldBe "14-OCT-24"
+
+      (json \ "nstpEndDate8")
+        .as[String] shouldBe "17-JUL-26"
+
+      (json \ "isInLastNstp")
+        .as[String] shouldBe "1"
+
+      (json \ "finalPeriodWarning")
+        .as[String] shouldBe "0"
+
+      (json \ "hasExistingNstpValues")
+        .as[String] shouldBe "1"
+
+      (json \ "systemDate")
+        .as[String] shouldBe "2026-05-31"
+    }
+
+    "return partial return periods for XJM00000000570" in {
+
+      val result =
+        controller.getReturnPeriods(
+          "mGd",
+          " XJM00000000570 "
+        )(FakeRequest())
+
+      status(result) shouldBe OK
+
+      val json =
+        contentAsJson(result)
+
+      (json \ "mgdRegNumber")
+        .as[String] shouldBe "XJM00000000570"
+
+      (json \ "returnPeriodsId")
+        .as[String] shouldBe "2"
+
+      (json \ "nstpEndDate1")
+        .as[String] shouldBe "31-MAR-26"
+
+      (json \ "nstpEndDate2")
+        .as[String] shouldBe ""
+
+      (json \ "hasExistingNstpValues")
+        .as[String] shouldBe "1"
+    }
+
+    "return no data for unknown registration number" in {
+
+      val result =
+        controller.getReturnPeriods(
+          "MGD",
+          "XGM00000001763"
+        )(FakeRequest())
+
+      status(result) shouldBe OK
+
+      val json =
+        contentAsJson(result)
+
+      (json \ "mgdRegNumber")
+        .as[String] shouldBe ""
+
+      (json \ "returnPeriodsId")
+        .as[String] shouldBe ""
+
+      (json \ "nstpEndDate1")
+        .as[String] shouldBe ""
+
+      (json \ "systemDate")
+        .as[String] shouldBe ""
+    }
+
+    "return BAD_REQUEST for an unrecognised regime" in {
+
+      val regime = "nope"
+
+      val result =
+        controller.getReturnPeriods(
+          regime,
+          "XWM00000001770"
+        )(FakeRequest())
+
+      status(result) shouldBe BAD_REQUEST
+
+      contentAsJson(result) shouldBe Json.obj(
+        "code"    -> "INVALID_REGIME",
+        "message" -> s"Regime $regime is not supported for Return Periods"
+      )
+    }
+
+    "return BAD_REQUEST for an unsupported regime" in {
+
+      val regime = "PBD"
+
+      val result =
+        controller.getReturnPeriods(
+          regime,
+          "XWM00000001770"
+        )(FakeRequest())
+
+      status(result) shouldBe BAD_REQUEST
+
+      contentAsJson(result) shouldBe Json.obj(
+        "code"    -> "INVALID_REGIME",
+        "message" -> s"Regime $regime is not supported for Return Periods"
+      )
+    }
+
+    "return BadRequest for XGM00000000560" in {
+
+      val result =
+        controller.getReturnPeriods(
+          "MGD",
+          "XGM00000000560"
+        )(FakeRequest())
+
+      status(result) shouldBe BAD_REQUEST
+
+      contentAsJson(result) shouldBe Json.obj(
+        "code"    -> "INVALID_REQUEST",
+        "message" -> "Bad request"
+      )
+    }
+
+    "return Unauthorized for XMM00000000580" in {
+
+      val result =
+        controller.getReturnPeriods(
+          "MGD",
+          "XMM00000000580"
+        )(FakeRequest())
+
+      status(result) shouldBe UNAUTHORIZED
+
+      contentAsJson(result) shouldBe Json.obj(
+        "code"    -> "UNAUTHORIZED",
+        "message" -> "Unauthorized to access this resource"
+      )
+    }
+
+    "return InternalServerError for XAM00000001090" in {
+
+      val result =
+        controller.getReturnPeriods(
+          "MGD",
+          "XAM00000001090"
+        )(FakeRequest())
+
+      status(result) shouldBe INTERNAL_SERVER_ERROR
+
+      contentAsJson(result) shouldBe Json.obj(
+        "code"    -> "UNEXPECTED_ERROR",
+        "message" -> "Unexpected error occurred"
+      )
+    }
+  }
+}
